35e7a5196b9c266a7451942a855db749484c3acf
Molerodev <alejandromolero.developer@gmail.com>
2025-05-27 13:30:14 +0200

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
 
 import { routes } from "./app.routes";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
     // otros providers que necesites
   ],
 };
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from "rxjs";
   providedIn: "root",
 })
 export class UserService {
-  private apiUrl = "http://localhost:8080/api/zave/user"; // URL del backend
+  private apiUrl = "https://zave.up.railway.app/api/zave/user"; // URL del backend
 
   constructor(private http: HttpClient) {}
 
