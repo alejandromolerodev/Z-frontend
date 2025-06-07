68b037f7222666b7f8aed89a877c04ae3b5aeb3d
Molerodev <alejandromolero.developer@gmail.com>
2025-06-07 13:34:01 +0200

    comments, mejorar code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,21 +3,15 @@ import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
 
 import { routes } from "./app.routes";
-import { provideServiceWorker } from '@angular/service-worker';
+import { provideServiceWorker } from "@angular/service-worker";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
-    // otros providers que necesites
+    provideHttpClient(),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "http://192.168.1.27:8080", // Usa aquí tu IP local y puerto
+  apiUrl: "http://172.20.10.3:8080", // Usa aquí tu IP local y puerto
 };
